512acfa6797ddaa88c80256513dbd93825db3eed
Nerd0Vidhan <vidhantiwari110@gmail.com>
2025-04-08 13:50:14 +0530

    SyAi version 0.0.1 setting up project

---
+++ app/src/main/java/com/mato/syai/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.mato.syai
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RiveAnimationTheme {
+                val systemUiController = rememberSystemUiController()
+                SideEffect {
+                    // set transparent color so that our image is visible
+                    // behind the status bar
+                    systemUiController.setStatusBarColor(color = Color(0xFFd7e1e8))
+                    systemUiController.setNavigationBarColor(
+                        color = Color(0xFFd7e1e8)
+                    )
+                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginUI()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/mato/syai/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.mato.syai.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PurpleDark = Color(0xFF00005B)
+val BrownLight=Color(0xFFF8E0C3)
+val White=Color(0xFFFFFFFF)
